bb761ef3459af19cbeeb15cb6afbd969da2694b5
gizmore <gizmore@wechall.net>
2022-07-20 23:22:16 +0200

    '7.0.0-r1377'

--- tpl/cell_pmfromto.php
+++ tpl/cell_pmfromto.php
@@ -2,7 +2,7 @@
 use GDO\PM\GDT_PMFromTo;
 use GDO\PM\GDO_PM;
 use GDO\User\GDO_User;
-use GDO\Profile\GDT_ProfileLink;
+use GDO\User\GDT_ProfileLink;
 /** @var $field GDT_PMFromTo **/
 /** @var $pm GDO_PM **/
 $user = GDO_User::current();
