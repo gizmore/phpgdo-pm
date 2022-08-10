dcff95a05a69a4820db355d2c8934b45d819fe85
gizmore <gizmore@wechall.net>
2022-08-10 05:56:07 +0200

    '7.0.1-r1416'

--- Method/Folders.php
+++ Method/Folders.php
@@ -13,7 +13,7 @@ final class Folders extends MethodTable
 	public function isFiltered() { return false; }
 	public function isPaginated() { return false; }
 	public function isUserRequired() : bool { return true; }
-	
+
 	public function getDefaultOrder() : ?string { return 'pmf_id'; }
 	
 	public function gdoTable() : GDO
@@ -24,16 +24,12 @@ final class Folders extends MethodTable
 	public function gdoHeaders() : array
 	{
 		$table = GDO_PMFolder::table();
-		return array(
+		return [
 		    $table->gdoColumn('pmf_id')->hidden(),
 			GDT_Template::make()->template('PM', 'folder_link.php')->label('folder'),
 			$table->gdoColumn('pmf_count'),
-		);
+		];
 	}
-	
-// 	protected function setupTitlePrefix()
-// 	{
-// 	}
 	
 	public function getResult() : ArrayResult
 	{
