2f8e66dd02d0cc69bc999270accbff04944f4098
gizmore <gizmore@wechall.net>
2022-10-02 09:05:57 +0200

    ''

--- tpl/filter_pmfromto.php
+++ tpl/filter_pmfromto.php
@@ -1,8 +1,9 @@
 <?php
 namespace GDO\PM\tpl;
 /** @var $field \GDO\PM\GDT_PMFromTo **/
+/** @var $f \GDO\Table\GDT_Filter **/
 ?>
 <input
  name="f[<?= $field->getName()?>]"
  type="text"
- value="<?= html($field->filterVar()); ?>" />
+ value="<?= html($field->filterVar($f)); ?>" />
