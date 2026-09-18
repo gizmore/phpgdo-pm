94282de3872eb27b2a9ea4c24a2165d60d59d3a9
gizmore <gizmore@wechall.net>
2026-09-18 04:05:36 +0200

    'more korean'

---
+++ lang/pm_ko.php
@@ -0,0 +1,105 @@
+<?php
+namespace GDO\PM\lang;
+
+return [
+	'module_pm' => '오후',
+	'cfg_pm_re' => '응답 제목 접두어',
+	'cfg_pm_limit' => 'PM 제한 시간 초과 내의 최대 PM',
+	'cfg_pm_limit_timeout' => 'PM 제한 시간 초과',
+	'cfg_pm_max_folders' => '최대 사용자 정의 폴더',
+	'cfg_pm_for_guests' => '게스트가 PM을 작성하도록 허용',
+	'cfg_pm_captcha' => 'PM을 작성하려면 보안 문자를 활성화하세요.',
+	'cfg_pm_causes_mail' => 'PM에서 메일 활성화',
+	'cfg_pm_bot_uid' => '봇 사용자 ID',
+	'cfg_pm_own_bot' => '비시스템 PM 봇을 사용하시겠습니까?',
+	'cfg_pm_per_page' => '페이지당 오후',
+	'cfg_pm_welcome' => '환영 PM을 쓰시겠습니까?',
+	'cfg_pm_sig_len' => '최대 서명 길이',
+	'cfg_pm_msg_len' => '최대 메시지 길이',
+	'cfg_pm_title_len' => '최대 제목 길이',
+	'cfg_pm_fname_len' => '최대 폴더 이름 길이',
+	'cfg_pm_delete' => 'PM을 삭제하시겠습니까?',
+	'cfg_pm_limit_per_level' => '최대 PM을 1개 더 늘릴 수 있는 레벨',
+	'cfg_pm_level' => '당신에게 메시지를 보내는 데 필요한 사용자 수준',
+	'cfg_pm_email' => 'PM을 받으면 메일을 보내시겠습니까?',
+	'cfg_pm_guests' => '손님이 메시지를 보내도록 허용하시겠습니까?',
+	'cfg_signature' => '당신의 서명',
+	'link_pm_center' => '고토 PM 개요',
+	'link_settings' => '설정',
+	'link_trashcan' => '휴지통',
+	'link_write_pm' => '새 PM 작성',
+	'name_trashcan' => '휴지통',
+	'inbox_name' => '수신',
+	'outbox_name' => '보냄',
+	'btn_restore' => '복원',
+	'btn_empty' => '비어 있음',
+	'btn_pm' => '비공개 메시지',
+	'btn_pm_unread' => '비공개 메시지 [%s]',
+	'msg_pm_destroyed' => '%s PM이 마침내 삭제되었습니다.',
+	'msg_pm_restored' => '%s PM이 복원되었습니다.',
+	'mt_pm_write' => '비공개 메시지 작성',
+	'msg_pm_sent' => '귀하의 비공개 메시지가 전송되었습니다.',
+	'err_pm_limit_reached' => '%s 내의 %s 메시지 PM 제한을 초과했습니다.',
+	'err_no_pm_self' => '자신에게 PM을 쓸 수는 없습니다.',
+	'err_only_pm_users' => '이 사용자에게 메시지를 보낼 수 없습니다.',
+	'pm_email' => '오후에 이메일',
+	'pm_level' => '당신을 PM하려면 사용자 레벨이 필요합니다',
+	'signature' => '당신의 서명',
+	'pm_guests' => '손님이 메시지를 보낼 수도 있습니다',
+	'pm_by' => '보낸 사람: %s, %s; %s',
+	'pm_to' => '받는 사람: %s, %s; %s',
+	'pm_from' => '%s부터, %s 전',
+	'pm_sent' => '보냈습니다. %s',
+	'pm_read' => '%s 전에 읽음',
+	'pm_unread' => '읽지 않음',
+	'btn_move' => '폴더로 이동',
+	'msg_pm_deleted' => '총 %s개의 PM이 삭제되었습니다.',
+	'err_pm_folder' => 'PM 폴더를 찾을 수 없습니다.',
+	'err_pm' => '이 PM을 찾을 수 없습니다.',
+	'pm_received' => '받았습니다, %s',
+	'pm_folder' => '%s 폴더(%s 메시지 포함)',
+	'from_user' => '에서',
+	'to_user' => '받는 사람',
+	'delete' => '삭제',
+	'reply' => '답장하다',
+	'quote' => '견적',
+	'show' => '쇼',
+	'mt_pm_delete' => '오후 삭제',
+	'list_pm_folders' => '%s 폴더',
+	'pm_welcome_title' => '%s에 오신 것을 환영합니다',
+	'pm_welcome_message' => '안녕하세요 %s님,
+	
+%s에서 귀하를 정중하게 환영하기를 바랍니다.
+사이트와 세션을 즐기시기 바랍니다.
+	
+친절한 감사
+%2$s 팀',
+	'msg_pm_mail_sent' => '%s에게도 이메일을 보냈습니다.',
+	'mail_subj_pm' => '%s: %s의 오후',
+	'mail_body_pm' => '안녕하세요 %s님,
+	
+%s 사용자가 %s에 비공개 메시지를 보냈습니다.
+	
+제목: %s
+	
+======================================================
+	
+%s
+	
+======================================================
+	
+한 번의 클릭으로 이 메시지를 삭제할 수 있습니다: %s
+	
+친절한 감사
+%3$s 팀',
+	'list_pm_trashcan' => '휴지통에 메시지가 없습니다.',
+	'mt_pm_reply' => '답장하다 PM',
+	'mt_pm_folder' => 'PM 폴더',
+	'mt_pm_folders' => 'PM 폴더',
+	'mt_pm_trashcan' => '오후 휴지통',
+	'list_pm_folder' => '%s PM(%s)',
+	'mt_pm_resendwelcome' => '환영 메시지 다시 보내기',
+	'msg_welcome_pm_resent' => '귀하의 환영 PM이 재전송되었습니다.',
+	'div_info_privacy_pm' => '비공개 메시지는 암호화되지 않으며 관리자가 읽을 수 있습니다. 현재는 다른 옵션이 없습니다.',
+	'recipient' => '받는 사람',
+];
